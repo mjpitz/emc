8e11da51d53e31e434754f0e0cd2fcc098c5c104
Mya Pitzeruse <j.mya.pitz@gmail.com>
2022-05-23 21:54:12 -0500

    examples: add an example catalog to demonstrate capabilities

---
+++ examples/catalog.go
@@ -0,0 +1,94 @@
+package main
+
+import (
+	"github.com/mjpitz/emc/catalog"
+	"github.com/mjpitz/emc/catalog/linkgroup"
+	"github.com/mjpitz/emc/catalog/service"
+)
+
+func main() {
+	catalog.Serve(
+		catalog.Service(
+			"CI/CD",
+			service.LogoURL("https://th.bing.com/th/id/OIP.wd0WnO0MF56eQ23LR8XzRAAAAA?pid=ImgDet&rs=1"),
+			service.URL("https://deploy.example.com"),
+			service.Description("Continuous integration and delivery platform."),
+			service.Metadata(
+				"Key1", "Key1Value1",
+				"Key1", "Key1Value2",
+				"Key2", "Key2Value1",
+			),
+			service.LinkGroup(
+				"Dashboards",
+				linkgroup.Link("System", "#"),
+				linkgroup.Link("Queue", "#"),
+			),
+			service.LinkGroup(
+				"Documentation",
+				linkgroup.Link("Developing", "#"),
+				linkgroup.Link("Releasing", "#"),
+				linkgroup.Link("Contributing", "#"),
+			),
+		),
+		catalog.Service(
+			"Version Control",
+			service.LogoURL(""),
+			service.URL("https://code.example.com"),
+			service.Description("System source code."),
+			service.Metadata(
+				"Key1", "Key1Value1",
+				"Key1", "Key1Value2",
+				"Key2", "Key2Value1",
+			),
+			service.LinkGroup(
+				"Dashboards",
+				linkgroup.Link("System", "#"),
+				linkgroup.Link("Database", "#"),
+				linkgroup.Link("Queue", "#"),
+			),
+			service.LinkGroup(
+				"Documentation",
+				linkgroup.Link("Developing", "#"),
+				linkgroup.Link("Releasing", "#"),
+			),
+		),
+		catalog.Service(
+			"Monitoring",
+			service.Metadata(
+				"Key1", "Key1Value1",
+				"Key1", "Key1Value2",
+				"Key2", "Key2Value1",
+			),
+			service.LinkGroup(
+				"Dashboards",
+				linkgroup.Link("System", "#"),
+				linkgroup.Link("Database", "#"),
+				linkgroup.Link("Queue", "#"),
+			),
+			service.LinkGroup(
+				"Documentation",
+				linkgroup.Link("Developing", "#"),
+				linkgroup.Link("Releasing", "#"),
+			),
+		),
+		catalog.Service(
+			"Alerting",
+			service.Metadata(
+				"Key1", "Key1Value1",
+				"Key1", "Key1Value2",
+				"Key2", "Key2Value1",
+			),
+			service.LinkGroup(
+				"Dashboards",
+				linkgroup.Link("System", "#"),
+				linkgroup.Link("Database", "#"),
+				linkgroup.Link("Queue", "#"),
+			),
+			service.LinkGroup(
+				"Documentation",
+				linkgroup.Link("Developing", "#"),
+				linkgroup.Link("Releasing", "#"),
+			),
+		),
+	)
+}
